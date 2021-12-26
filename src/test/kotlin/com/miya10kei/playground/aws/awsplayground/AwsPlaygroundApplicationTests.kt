e8be91ce5c0e2fb91ecc34a00bfc010204ee4cdd
miya10kei <miya10kei@gmail.com>
2021-12-26 15:16:39 +0900

    :tada: Initial commit

---
+++ src/test/kotlin/com/miya10kei/playground/aws/awsplayground/AwsPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miya10kei.playground.aws.awsplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
